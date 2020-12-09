cce1d3f0bf91914b5a69d9705ee635a69168064d
rogerschuster <61434884+rogerschuster@users.noreply.github.com>
2020-12-09 23:33:24 +0100

    JUnit 5 Support

--- compass2therion/src/test/java/de/r_schuster/AppTest.java
+++ compass2therion/src/test/java/de/r_schuster/AppTest.java
@@ -1,8 +1,11 @@
 package de.r_schuster;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+
+
+
 
 /**
  * Unit test for simple App.
