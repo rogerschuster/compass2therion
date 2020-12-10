16f6c97ef5f26e36a40e761d259e6c6a10cbbca7
rogerschuster <61434884+rogerschuster@users.noreply.github.com>
2020-12-10 22:40:35 +0100

    Parser

---
+++ compass2therion/src/main/java/de/r_schuster/parser/CompassDatParser.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 roger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.r_schuster.parser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+/**
+ *
+ * @author roger
+ */
+public class CompassDatParser {
+
+    public void parse(InputStream is, Charset charset) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is, charset));
+
+        String line = reader.readLine();
+        int no = 1;
+        while (line != null) {
+
+            no++;
+            line = reader.readLine();
+        }
+    }
+}
