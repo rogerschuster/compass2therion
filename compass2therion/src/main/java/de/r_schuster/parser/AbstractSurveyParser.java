759dd3e7e1ca686fa0fa203c9c869e4283922a34
rogerschuster <61434884+rogerschuster@users.noreply.github.com>
2020-12-13 19:37:04 +0100

    converting

--- compass2therion/src/main/java/de/r_schuster/parser/AbstractSurveyParser.java
+++ compass2therion/src/main/java/de/r_schuster/parser/AbstractSurveyParser.java
@@ -16,10 +16,31 @@
  */
 package de.r_schuster.parser;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 /**
  *
  * @author roger
  */
 public abstract class AbstractSurveyParser implements SurveyParser {
-    
+
+    private static final BigDecimal TO_GRADIANS = BigDecimal.valueOf(400.00).divide(BigDecimal.valueOf(360.00), MathContext.DECIMAL32);
+    private static final BigDecimal TO_METRES = BigDecimal.valueOf(0.3048);
+
+    protected BigDecimal degreeToGradians(BigDecimal degree) {
+        return degree.setScale(2, RoundingMode.HALF_UP).multiply(TO_GRADIANS).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    protected BigDecimal degreeToPercent(BigDecimal degree) {
+        double toRadians = Math.toRadians(degree.doubleValue());
+        double percent = Math.tan(toRadians) * 100;
+        return BigDecimal.valueOf(percent).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    protected BigDecimal decimalFeetToMetres(BigDecimal feet) {
+        return feet.multiply(TO_METRES, MathContext.DECIMAL32).setScale(2, RoundingMode.HALF_UP);
+    }
+
 }
