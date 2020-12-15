4429488187786b87699fc55b631811b62f3d50aa
rogerschuster <61434884+rogerschuster@users.noreply.github.com>
2020-12-15 21:10:44 +0100

    command line info message

--- compass2therion/src/main/java/de/r_schuster/App.java
+++ compass2therion/src/main/java/de/r_schuster/App.java
@@ -13,21 +13,37 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.util.logging.Logger;
 
 public class App {
-    
+
+    private static final Logger LOG = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) throws IOException {
-        File outputfile = new File(args[2]);
-        if (outputfile.exists()) {
-            throw new IOException("File " + args[2] + " exists!");
+        if (args.length != 3) {
+            LOG.warning("Application expects 3 parameters:");
+            LOG.warning("Inputfile Outputfile Cavename");
+            LOG.warning("Inputfile: Path to a file in Compass format");
+            LOG.warning("Outputfile: Path to a file in Therion format");
+            LOG.warning("Cavename: Name of the cave (in quotation marks if it contains blanks or non-alphanumeric characters)");
+            return;
         }
-        
+
+        String in = args[0];
+        String out = args[1];
+        String name = args[2];
+
+        File outputfile = new File(out);
+        if (outputfile.exists()) {
+            throw new IOException("File " + out + " already exists!");
+        }
+
         SurveyParser parser = new CompassParser();
         Networking networking = new FlatNetworking();
-        Cave cave = parser.parse(args[0], new File(args[1]), Charset.forName("Cp1252"), networking);
-        
-        try (Writer out = new OutputStreamWriter(new FileOutputStream(outputfile), Charset.forName("UTF-8"))) {
-            SurveyWriter writer = new TherionWriter(out);
+        Cave cave = parser.parse(name, new File(in), Charset.forName("Cp1252"), networking);
+
+        try (Writer wrt = new OutputStreamWriter(new FileOutputStream(outputfile), Charset.forName("UTF-8"))) {
+            SurveyWriter writer = new TherionWriter(wrt);
             writer.write(Charset.forName("UTF-8"), cave);
         }
     }
